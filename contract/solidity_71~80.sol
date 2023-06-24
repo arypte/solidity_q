e2f4ebd89553bfb587472569fcdf1b329237ca78
arypte <wjs505@gmail.com>
2023-06-24 23:50:11 +0900

    ~Q79

--- contract/solidity_71~80.sol
+++ contract/solidity_71~80.sol
@@ -143,41 +143,121 @@ contract Q76{
 
         // 거꾸로 뒤집기.. 
 
+        ( rt_value[ 0 ] , rt_value[ 2 ] ) = ( rt_value[ 2 ] , rt_value[ 0 ] ) ;
+
         return string( rt_value ) ;
 
     }
 
 }
 
-/*
+//1. 위의 문제와 비슷합니다. 이번에는 openzeppelin의 패키지를 import 하세요.
+//    힌트 : import "@openzeppelin/contracts/utils/Strings.sol";
 
-1. 위의 문제와 비슷합니다. 이번에는 openzeppelin의 패키지를 import 하세요.
-    
-    힌트 : import "@openzeppelin/contracts/utils/Strings.sol";
-    
-- 답안
-    
-    ```solidity
-    
-    ```
-/*    
-1. 숫자만 들어갈 수 있는 array를 선언하세요. array 안 요소들 중 최소 하나는 10~25 사이에 있는지를 알려주는 함수를 구현하세요.
-    
-    예) [1,2,6,9,11,19] -> true (19는 10~25 사이) // [1,9,3,6,2,8,9,39] -> false (어느 숫자도 10~25 사이에 없음)
-    
-- 답안
-    
-    ```solidity
-    
-    ```
-    
-1. 3개의 숫자를 넣으면 그 중에서 가장 큰 수를 찾아내주는 함수를 Contract A에 구현하세요. Contract B에서는 이름, 번호, 점수를 가진 구조체 학생을 구현하세요. 학생의 정보를 3명 넣되 그 중에서 가장 높은 점수를 가진 학생을 반환하는 함수를 구현하세요. 구현할 때는 Contract A를 import 하여 구현하세요.
-- 답안
-    
-    ```solidity
-    
-    ```
-    
-1. 지금은 동적 array에 값을 넣으면(push) 가장 앞부터 채워집니다. 1,2,3,4 순으로 넣으면 [1,2,3,4] 이렇게 표현됩니다. 그리고 값을 빼려고 하면(pop) 끝의 숫자부터 빠집니다. 가장 먼저 들어온 것이 가장 마지막에 나갑니다. 이런 것들을FILO(First In Last Out)이라고도 합니다. 가장 먼저 들어온 것을 가장 먼저 나가는 방식을 FIFO(First In First Out)이라고 합니다. push와 pop을 이용하되 FIFO 방식으로 바꾸어 보세요.
-- 답안
-*/
+import "@openzeppelin/contracts/utils/Strings.sol" ;
+contract Q77{
+
+    // using <library> for <type>;
+    using Strings for uint ;
+
+    function A( uint _input ) public pure returns( string memory ) {
+
+        return _input.toString() ;
+        // ?? toString( _input ) ; 아님??
+        // 라이브러리 공부 체크
+
+    }
+
+}
+
+// 1. 숫자만 들어갈 수 있는 array를 선언하세요. array 안 요소들 중 최소 하나는 10~25 사이에 있는지를 알려주는 함수를 구현하세요.
+// 예) [1,2,6,9,11,19] -> true (19는 10~25 사이) // [1,9,3,6,2,8,9,39] -> false (어느 숫자도 10~25 사이에 없음)
+
+contract Q78{ 
+
+    uint[] array ;
+
+    function Apush( uint _input ) public {
+        array.push( _input ) ;
+    }
+
+    function getB() public view returns( bool ) {
+
+        for( uint i = 0 ; i < array.length ; i ++ ) {
+
+            if( array[ i ] > 9 && array[ i ] < 26 ) return true ; 
+
+        }
+
+        return false ;
+
+    }
+
+}
+
+// 1. 3개의 숫자를 넣으면 그 중에서 가장 큰 수를 찾아내주는 함수를 Contract A에 구현하세요.
+// Contract B에서는 이름, 번호, 점수를 가진 구조체 학생을 구현하세요.
+// 학생의 정보를 3명 넣되 그 중에서 가장 높은 점수를 가진 학생을 반환하는 함수를 구현하세요.
+// 구현할 때는 Contract A를 import 하여 구현하세요.
+
+import "contract/solidity_71~80.sol" ;
+
+contract Q79_A{
+
+    function findMax( uint _a , uint _b , uint _c ) public pure returns( uint ){
+
+        uint rt = _a > _b ? _a : _b ;
+        rt = rt > _c ? rt : _c ;
+        return rt == _a ? 0 : ( rt == _b ? 1 : 2 ) ;
+
+    }
+
+    function re() public view returns( address ){
+        return msg.sender ;
+    }
+
+}
+
+contract Q79_B{
+
+    struct student{
+
+        string name ;
+        uint number ;
+        uint score ;
+
+    }
+
+    student[] db ;
+
+    Q79_A public A = new Q79_A() ;
+    // 완전히 새로운 컨트렉트가 선언되는거 같다..?
+    // 가스비 2번 먹는지 체크
+
+    function s_push( string memory name , uint score ) public {
+
+        db.push( student( name , db.length + 1 , score ) ) ;
+
+    }
+
+    function f( uint _a , uint _b , uint _c ) public view returns( student memory ) {
+        uint t = A.findMax( db[ _a ].score , db[ _b ].score , db[ _c ].score ) ;
+        return t == 0 ? db[ _a ] : ( t == 1 ? db[ _b ] : db[ _c ] ) ;
+    }
+
+    function T() public view returns( address ){
+        return A.re() ;
+    }
+    // 상속, msg.sender 공부를 좀 더 해야할듯
+
+}
+
+
+// 1. 지금은 동적 array에 값을 넣으면(push) 가장 앞부터 채워집니다.
+//  1,2,3,4 순으로 넣으면 [1,2,3,4] 이렇게 표현됩니다. 그리고 값을 빼려고 하면(pop) 끝의 숫자부터 빠집니다.
+//  가장 먼저 들어온 것이 가장 마지막에 나갑니다. 이런 것들을FILO(First In Last Out)이라고도 합니다.
+//  가장 먼저 들어온 것을 가장 먼저 나가는 방식을 FIFO(First In First Out)이라고 합니다. push와 pop을 이용하되 FIFO 방식으로 바꾸어 보세요.
+
+contract Q80{
+
+}
